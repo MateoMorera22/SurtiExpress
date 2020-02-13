ce05feadfc5f1c3474b369be24be9b14ef422b1a
Michael Farfan <farfan.avilams@gmail.com>
2020-02-12 22:15:47 -0500

    metodo pruvba

--- src/java/Controladores/estadotareaControlador.java
+++ src/java/Controladores/estadotareaControlador.java
@@ -41,4 +41,7 @@ public class estadotareaControlador implements Serializable {
     public List<Estadotarea> listarEstadoTarea() {
         return estadotareaFacade.findAll();
     }
+    public void crear(){
+        
+    }
 }
