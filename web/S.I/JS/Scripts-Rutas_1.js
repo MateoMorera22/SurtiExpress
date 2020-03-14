b96796e1a6c3e25d95463ac3d6876611df014e1f
Mateo Morera <moreramateo@gmail.com>
2020-03-13 23:11:01 -0500

    Agregue el projecto de nuevo

---
+++ web/S.I/JS/Scripts-Rutas_1.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function () {
+    $('.collapsible').collapsible();
+});
+
+$(document).ready(function () {
+    $('.modal').modal();
+});
+
+$(document).ready(function () {
+    $('select').formSelect();
+});
+
