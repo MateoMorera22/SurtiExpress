59f5bcac720c0c5912c97db4381521cc21e87618
Mateo Morera <moreramateo@gmail.com>
2020-03-13 22:50:00 -0500

    Mande todos los build

--- build/generated-sources/ap-source-output/Entidades/Estadotarea_.java
+++ build/generated-sources/ap-source-output/Entidades/Estadotarea_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-03-12T08:27:04")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-03-13T22:40:28")
 @StaticMetamodel(Estadotarea.class)
 public class Estadotarea_ { 
 
